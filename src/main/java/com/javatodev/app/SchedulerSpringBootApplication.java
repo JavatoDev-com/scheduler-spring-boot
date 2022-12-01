3ec24121b8fc5cf5ab2dcbff8bc7a6539bbf8613
chinthaka-dinadasa <sendtoclabz@gmail.com>
2022-12-01 16:42:19 +0530

    initial commit

---
+++ src/main/java/com/javatodev/app/SchedulerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerSpringBootApplication.class, args);
+	}
+
+}
